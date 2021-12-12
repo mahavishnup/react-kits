78033ae46706f790e83f5824404bd479bd6c2cb0
mahavishnup <selvamvishnu25@gmail.com>
2021-12-12 14:01:16 +0530

    three skeleton design update

--- src/components/skeletonLoader/Main.js
+++ src/components/skeletonLoader/Main.js
@@ -1,13 +1,15 @@
 import React from "react";
 import One from "./design/One";
+import Three from "./design/Three";
 import Two from "./design/Two";
 
 const Main = () => {
   return (
     <>
-      <div class="flex-1 flex p-10">
+      <div class="min-h-screen flex-1 flex p-10">
         <One />
         <Two />
+        <Three />
       </div>
     </>
   );
---
+++ src/components/skeletonLoader/design/Three.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Three = () => {
+    return (
+      <>
+        <div class="w-1/3">
+          <div class="flex items-center justify-center">
+            <div class="w-64 bg-white rounded shadow-2xl">
+              <div class="h-32 bg-gray-200 rounded-tr rounded-tl animate-pulse"></div>
+
+              <div class="p-5">
+                <div class="h-6 rounded-sm bg-gray-200 animate-pulse mb-4"></div>
+
+                <div class="grid grid-cols-4 gap-1">
+                  <div class="col-span-3 h-4 rounded-sm bg-gray-200 animate-pulse"></div>
+                  <div class="h-4 rounded-sm bg-gray-200 animate-pulse"></div>
+
+                  <div class="col-span-2 h-4 rounded-sm bg-gray-200 animate-pulse"></div>
+                  <div class="col-span-2 h-4 rounded-sm bg-gray-200 animate-pulse"></div>
+
+                  <div class="h-4 rounded-sm bg-gray-200 animate-pulse"></div>
+                  <div class="col-span-3 h-4 rounded-sm bg-gray-200 animate-pulse"></div>
+                  <div class="col-span-2 h-4 rounded-sm bg-gray-200 animate-pulse"></div>
+                  <div class="h-4 rounded-sm bg-gray-200 animate-pulse"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+}
+
+export default Three
