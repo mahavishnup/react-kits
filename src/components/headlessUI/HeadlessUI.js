b83fdb1ee170bf7ca9f14424cea0a8c25bcd0361
mahavishnup <selvamvishnu25@gmail.com>
2021-12-12 18:22:27 +0530

    headlessui

--- src/components/headlessUI/HeadlessUI.js
+++ src/components/headlessUI/HeadlessUI.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./headlessUI.css";
 
 const HeadlessUI = () => {
   return (
     <>
-      <h1>Headless UI</h1>
+      <nav className="nav flex-1 flex justify-items-center">
+        <Link className="link" to="/headlessui/dialog">
+          Dialog
+        </Link>
+        <Link className="link" to="/headlessui/disclosure">
+          Disclosure
+        </Link>
+        <Link className="link" to="/headlessui/listbox">
+          Listbox
+        </Link>
+        <Link className="link" to="/headlessui/menu">
+          Menu
+        </Link>
+        <Link className="link" to="/headlessui/popover">
+          Popover
+        </Link>
+        <Link className="link" to="/headlessui/radiogroup">
+          Radio Group
+        </Link>
+        <Link className="link" to="/headlessui/switch">
+          Switch
+        </Link>
+        <Link className="link" to="/headlessui/tabs">
+          Tabs
+        </Link>
+        <Link className="link" to="/headlessui/transition">
+          Transition
+        </Link>
+      </nav>
     </>
   );
 };
--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -2,6 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HeadlessUI from "../headlessUI/HeadlessUI";
 import SkeletonLoader from "../skeletonLoader/SkeletonLoader";
+import DialogBox from "../headlessUI/design/DialogBox";
+import DisclosureBox from "../headlessUI/design/DisclosureBox";
+import ListBox from "../headlessUI/design/ListBox";
+import MenuBox from "../headlessUI/design/MenuBox";
+import PopoverBox from "../headlessUI/design/PopoverBox";
+import RadioGroupBox from "../headlessUI/design/RadioGroupBox";
+import SwitchBox from "../headlessUI/design/SwitchBox";
+import TabsBox from "../headlessUI/design/TabsBox";
+import TransitionBox from "../headlessUI/design/TransitionBox";
 
 const Router = () => {
   return (
@@ -9,6 +18,15 @@ const Router = () => {
       <Routes>
         <Route path="/" exact element={<SkeletonLoader />} />
         <Route path="/headlessui" element={<HeadlessUI />} />
+        <Route path="/headlessui/dialog" element={<DialogBox />} />
+        <Route path="/headlessui/disclosure" element={<DisclosureBox />} />
+        <Route path="/headlessui/listbox" element={<ListBox />} />
+        <Route path="/headlessui/menu" element={<MenuBox />} />
+        <Route path="/headlessui/popover" element={<PopoverBox />} />
+        <Route path="/headlessui/radiogroup" element={<RadioGroupBox />} />
+        <Route path="/headlessui/switch" element={<SwitchBox />} />
+        <Route path="/headlessui/tabs" element={<TabsBox />} />
+        <Route path="/headlessui/transition" element={<TransitionBox />} />
       </Routes>
     </>
   );
